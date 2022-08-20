c3d59501a153f2b3f14c3e06ded267a9a64b9820
Eric Butera <eric@ericbutera.com>
2022-08-20 17:50:10 -0400

    fix: config should be optional with defaults

--- storage-api/config/config.go
+++ storage-api/config/config.go
@@ -26,15 +26,23 @@ func NewAppConfig(path *string, file *string) (AppConfig, error) {
 	viper.SetConfigName(*file)
 	viper.SetConfigType("yaml")
 
+	viper.SetDefault("jaeger", false)
+	viper.SetDefault("data_dog", false)
+	viper.SetDefault("data_dog_api_key", "")
+	viper.SetDefault("app_name", "Storage-API")
+	viper.SetDefault("service_name", "storage-api")
+	viper.SetDefault("env", "dev")
+	viper.SetDefault("version", "0.0.1")
+
 	read_err := viper.ReadInConfig()
 	if read_err != nil {
-		panic(fmt.Errorf("fatal error config file: %w", read_err))
+		log.Print(fmt.Errorf("fatal error config file: %w", read_err))
 	}
 
 	var config AppConfig
 	parse_err := viper.Unmarshal(&config)
 	if parse_err != nil {
-		panic(fmt.Errorf("cannot parse config %s", parse_err))
+		log.Print(fmt.Errorf("cannot parse config %s", parse_err))
 	}
 
 	log.Printf("config %+v", config)
